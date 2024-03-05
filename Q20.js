af8587395fd97cdde1e7189c6af1bec89620efef
Madiha Khan <159880897+khan786madiha@users.noreply.github.com>
2024-03-05 10:34:54 -0800

    Q-20 Ts/Js & node.js of 45 assignments.
    Madiha Khan's NODEJS 45 Assignment Q-20

---
+++ Q20.js
@@ -0,0 +1,16 @@
+"use strict";
+// 20.	Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+Object.defineProperty(exports, "__esModule", { value: true });
+let languages = [
+    "Urdu",
+    "English",
+    "Birtish",
+    "Spanish",
+    "French",
+    "German",
+    "Arabic",
+    "Russian",
+    "Hindi",
+    "Bengali",
+];
+languages.map((item) => console.log(item));
